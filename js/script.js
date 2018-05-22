1b9029ce10a0c114bfff2de16ab3d6dbd10ef3cd
Khristian Marcial <khristianmarcial@gmail.com>
2018-05-22 13:13:58 -0400

    Added cool typey thingy

--- js/script.js
+++ js/script.js
@@ -2,14 +2,16 @@ $(document).ready(function(){
     // Add smooth scrolling to all links
     $("a").on('click', function(event) {
   
+        //checks for hash to prevent default behavior
       if (this.hash !== "") {
         event.preventDefault();
         var hash = this.hash;
+        //jQuery method
         $('html, body').animate({
           scrollTop: $(hash).offset().top
         }, 800, function(){
      
-          // Add hash (#) to URL when done scrolling (default click behavior)
+          // Add hashtag thing
           window.location.hash = hash;
         });
       }
